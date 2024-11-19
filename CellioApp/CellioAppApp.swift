67887645366a80c476208000db0b40b6f9880ba4
rahul2227 <rahul2227.l115@gmail.com>
2024-11-19 12:08:15 +0100

    Initial Commit

---
+++ CellioApp/CellioAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  CellioAppApp.swift
+//  CellioApp
+//
+//  Created by Rahul Sharma on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CellioAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
